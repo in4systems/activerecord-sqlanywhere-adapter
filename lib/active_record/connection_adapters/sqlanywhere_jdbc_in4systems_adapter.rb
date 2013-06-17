2a423aa2a8b3e79ae8b9c9e01a100554ac925adc
Chris Couzens <chris.couzens@in4systems.com>
2013-06-17 10:53:38 +0100

    Clear whitespace on the ends of lines in the adapter code

--- lib/active_record/connection_adapters/sqlanywhere_jdbc_in4systems_adapter.rb
+++ lib/active_record/connection_adapters/sqlanywhere_jdbc_in4systems_adapter.rb
@@ -6,7 +6,7 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#                                                                               
+#
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 #
@@ -35,13 +35,13 @@ module ActiveRecord
     # Main connection function to SQL Anywhere
     # Connection Adapter takes four parameters:
     # * :database (required, no default). Corresponds to "DatabaseName=" in connection string
-    # * :server (optional, defaults to :databse). Corresponds to "ServerName=" in connection string 
+    # * :server (optional, defaults to :databse). Corresponds to "ServerName=" in connection string
     # * :username (optional, default to 'dba')
     # * :password (optional, deafult to 'sql')
     # * :encoding (optional, defaults to charset of OS)
     # * :commlinks (optional). Corresponds to "CommLinks=" in connection string
     # * :connection_name (optional). Corresponds to "ConnectionName=" in connection string
-    
+
     def self.sqlanywhere_jdbc_in4systems_connection(config)
 
       if config[:connection_string]
@@ -54,7 +54,7 @@ module ActiveRecord
         connection_string = "ServerName=#{(config[:server] || config[:database])};DatabaseName=#{config[:database]};UserID=#{config[:username]};Password=#{config[:password]};"
         connection_string += "CommLinks=#{config[:commlinks]};" unless config[:commlinks].nil?
         connection_string += "ConnectionName=#{config[:connection_name]};" unless config[:connection_name].nil?
-        connection_string += "CharSet=#{config[:encoding]};" unless config[:encoding].nil?      
+        connection_string += "CharSet=#{config[:encoding]};" unless config[:encoding].nil?
         connection_string += "Idle=0" # Prevent the server from disconnecting us if we're idle for >240mins (by default)
       end
 
@@ -93,7 +93,7 @@ module ActiveRecord
         @sql = sql
       end
     end
-  
+
     class SQLAnywhereColumn < Column
       private
         # Overridden to handle SQL Anywhere integer, varchar, binary, and timestamp types
@@ -114,12 +114,12 @@ module ActiveRecord
           case sql_type
             when /^tinyint/i
               1
-            when /^smallint/i 
+            when /^smallint/i
               2
-            when /^integer/i  
-              4            
-            when /^bigint/i   
-              8  
+            when /^integer/i
+              4
+            when /^bigint/i
+              8
             else super
           end
         end
@@ -131,16 +131,16 @@ module ActiveRecord
         def self.string_to_binary(value)
           "\\x" + value.unpack("H*")[0].scan(/../).join("\\x")
         end
-        
+
         def self.binary_to_string(value)
           value.gsub(/\\x[0-9]{2}/) { |byte| byte[2..3].hex }
         end
-		
+
 		# Should override the time column values.
 		# Sybase doesn't like the time zones.
-		
+
     end
-    
+
     class SQLAnywhereJdbcIn4systemsAdapter < AbstractAdapter
       delegate :select, :select_rows, :execute, to: :conn
 
@@ -150,10 +150,10 @@ module ActiveRecord
         @visitor = Arel::Visitors::SQLAnywhere.new self
         @conn = conn
       end
-      
+
       def self.visitor_for(pool)
         config = pool.spec.config
-        
+
         if config.fetch(:prepared_statements) {true}
           Arel::Visitors::SQLAnywhere.new pool
         else
@@ -172,7 +172,7 @@ module ActiveRecord
       def requires_reloading?
         true
       end
-   
+
       def active?
         # The liveness variable is used a low-cost "no-op" to test liveness
         SA.instance.api.sqlany_execute_immediate(@connection, "SET liveness = 1") == 1
@@ -252,7 +252,7 @@ module ActiveRecord
               when 1
                 column_type_sql = 'tinyint'
               when 2
-                column_type_sql = 'smallint'  
+                column_type_sql = 'smallint'
               when 3..4
                 column_type_sql = 'integer'
               when 5..8
@@ -265,18 +265,18 @@ module ActiveRecord
              "varchar (#{limit})"
           elsif type == :boolean
             column_type_sql = 'tinyint'
-          else 
+          else
             super(type, limit, precision, scale)
           end
         else
           super(type, limit, precision, scale)
         end
       end
-      
+
       def viewed_tables(name = nil)
         list_of_tables(['view'], name)
       end
-      
+
       def base_tables(name = nil)
         list_of_tables(['base'], name)
       end
@@ -291,13 +291,13 @@ module ActiveRecord
           SQLAnywhereColumn.new(field['name'], field['default'], field['domain'], (field['nulls'] == 1))
         end
       end
-      
+
       def indexes(table_name, name = nil) #:nodoc:
         sql = "SELECT DISTINCT index_name, \"unique\" FROM SYS.SYSTABLE INNER JOIN SYS.SYSIDXCOL ON SYS.SYSTABLE.table_id = SYS.SYSIDXCOL.table_id INNER JOIN SYS.SYSIDX ON SYS.SYSTABLE.table_id = SYS.SYSIDX.table_id AND SYS.SYSIDXCOL.index_id = SYS.SYSIDX.index_id WHERE table_name = '#{table_name}' AND index_category > 2"
         select(sql, name).map do |row|
           index = IndexDefinition.new(table_name, row['index_name'])
           index.unique = row['unique'] == 1
-          sql = "SELECT column_name FROM SYS.SYSIDX INNER JOIN SYS.SYSIDXCOL ON SYS.SYSIDXCOL.table_id = SYS.SYSIDX.table_id AND SYS.SYSIDXCOL.index_id = SYS.SYSIDX.index_id INNER JOIN SYS.SYSCOLUMN ON SYS.SYSCOLUMN.table_id = SYS.SYSIDXCOL.table_id AND SYS.SYSCOLUMN.column_id = SYS.SYSIDXCOL.column_id WHERE index_name = '#{row['index_name']}'"	
+          sql = "SELECT column_name FROM SYS.SYSIDX INNER JOIN SYS.SYSIDXCOL ON SYS.SYSIDXCOL.table_id = SYS.SYSIDX.table_id AND SYS.SYSIDXCOL.index_id = SYS.SYSIDX.index_id INNER JOIN SYS.SYSCOLUMN ON SYS.SYSCOLUMN.table_id = SYS.SYSIDXCOL.table_id AND SYS.SYSCOLUMN.column_id = SYS.SYSIDXCOL.column_id WHERE index_name = '#{row['index_name']}'"
           index.columns = select(sql).map { |col| col['column_name'] }
           index
         end
@@ -330,15 +330,15 @@ module ActiveRecord
           execute("UPDATE #{quote_table_name(table_name)} SET #{quote_column_name(column_name)}=#{quote(default)} WHERE #{quote_column_name(column_name)} IS NULL")
         end
         execute("ALTER TABLE #{quote_table_name(table_name)} ALTER #{quote_column_name(column_name)} #{null ? '' : 'NOT'} NULL")
-      end             
+      end
 
-      def change_column(table_name, column_name, type, options = {}) #:nodoc:         
+      def change_column(table_name, column_name, type, options = {}) #:nodoc:
         add_column_sql = "ALTER TABLE #{quote_table_name(table_name)} ALTER #{quote_column_name(column_name)} #{type_to_sql(type, options[:limit], options[:precision], options[:scale])}"
         add_column_options!(add_column_sql, options)
         add_column_sql << ' NULL' if options[:null]
         execute(add_column_sql)
       end
-       
+
       def rename_column(table_name, column_name, new_column_name) #:nodoc:
         if column_name.downcase == new_column_name.downcase
           whine = "if_the_only_change_is_case_sqlanywhere_doesnt_rename_the_column"
@@ -357,7 +357,7 @@ module ActiveRecord
             WHERE "column_name" = '#{unquoted_column_name}' AND "table_name" = '#{table_name}'
           SQL
           select(sql, nil).each do |row|
-            execute "DROP INDEX \"#{table_name}\".\"#{row['index_name']}\""      
+            execute "DROP INDEX \"#{table_name}\".\"#{row['index_name']}\""
           end
           execute "ALTER TABLE #{quote_table_name(table_name)} DROP #{column_name}"
         end
@@ -385,13 +385,13 @@ module ActiveRecord
       end
 
       protected
-      
+
         def list_of_tables(types, name = nil)
           sql = "SELECT table_name FROM SYS.SYSTABLE WHERE table_type in (#{types.map{|t| quote(t)}.join(', ')}) and creator NOT IN (0,3,5)"
           select(sql, name).map { |row| row["table_name"] }
         end
 
-        # Queries the structure of a table including the columns names, defaults, type, and nullability 
+        # Queries the structure of a table including the columns names, defaults, type, and nullability
         # ActiveRecord uses the type to parse scale and precision information out of the types. As a result,
         # chars, varchars, binary, nchars, nvarchars must all be returned in the form <i>type</i>(<i>width</i>)
         # numeric and decimal must be returned in the form <i>type</i>(<i>width</i>, <i>scale</i>)
@@ -400,9 +400,9 @@ module ActiveRecord
 
         def table_structure(table_name)
           sql = <<-SQL
-SELECT SYS.SYSCOLUMN.column_name AS name, 
+SELECT SYS.SYSCOLUMN.column_name AS name,
   if left("default",1)='''' then substring("default", 2, length("default")-2) // remove the surrounding quotes
-  else NULLIF(SYS.SYSCOLUMN."default", 'autoincrement') 
+  else NULLIF(SYS.SYSCOLUMN."default", 'autoincrement')
   endif AS "default",
   IF SYS.SYSCOLUMN.domain_id IN (7,8,9,11,33,34,35,3,27) THEN
     IF SYS.SYSCOLUMN.domain_id IN (3,27) THEN
@@ -411,12 +411,12 @@ SELECT SYS.SYSCOLUMN.column_name AS name,
       SYS.SYSDOMAIN.domain_name || '(' || SYS.SYSCOLUMN.width || ')'
     ENDIF
   ELSE
-    SYS.SYSDOMAIN.domain_name 
-  ENDIF AS domain, 
+    SYS.SYSDOMAIN.domain_name
+  ENDIF AS domain,
   IF SYS.SYSCOLUMN.nulls = 'Y' THEN 1 ELSE 0 ENDIF AS nulls
-FROM 
-  SYS.SYSCOLUMN 
-  INNER JOIN SYS.SYSTABLE ON SYS.SYSCOLUMN.table_id = SYS.SYSTABLE.table_id 
+FROM
+  SYS.SYSCOLUMN
+  INNER JOIN SYS.SYSTABLE ON SYS.SYSCOLUMN.table_id = SYS.SYSTABLE.table_id
   INNER JOIN SYS.SYSDOMAIN ON SYS.SYSCOLUMN.domain_id = SYS.SYSDOMAIN.domain_id
 WHERE
   SYS.SYSTABLE.creator = 1 AND
@@ -426,7 +426,7 @@ SQL
           raise(ActiveRecord::StatementInvalid, "Could not find table '#{table_name}'") if structure == false
           structure
         end
-        
+
         # Required to prevent DEFAULT NULL being added to primary keys
         def options_include_default?(options)
           options.include?(:default) && !(options[:null] == false && options[:default].nil?)
